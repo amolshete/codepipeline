dce16ab444408f1fbf9c65f61cbda92adc646203
Amol Shete <71395005+amolshete@users.noreply.github.com>
2025-06-21 12:43:06 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -7,4 +7,4 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 
-aws s3 cp s3://codebuild-demo-project-27th-may/app.jar /home/ubuntu/app.jar  #make sure to change the bucket name here
+aws s3 cp s3://codebuild-demo-21st-june-2025/app.jar /home/ubuntu/app.jar  #make sure to change the bucket name here
